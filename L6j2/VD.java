41502925fbe3150ea6e7604fda164d42b1c2b2cb
phong100401 <65002403+phong100401@users.noreply.github.com>
2020-11-11 07:03:02 +0700

    Update VD.java

--- L6j2/VD.java
+++ L6j2/VD.java
@@ -10,7 +10,16 @@
 //
 //                Statement stmt = conn.createStatement();
 //        ){
-//
+//step 3, 4
+            String strUpdate = "UPDATE books set price = price * 0.7, qty = qty + 1 where id = 1001";
+            System.out.println("The SQL statement is: " + strUpdate + "\n");
+            int countUpdate = stmt.executeUpdate(strUpdate);
+            System.out.println((countUpdate + " records affected.\n"));
+
+
+            String strSelect = "select * from books where id = 1001";
+            System.out.println("The SQL statement is: " + strSelect + "\n");
+            ResultSet rset = stmt.executeQuery(strSelect);
 //            while (rset.next()){
 //                System.out.println(rset.getInt("id") + ","
 //                        + rset.getString("title") + ","
